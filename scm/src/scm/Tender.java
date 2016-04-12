4c15bd37913ba96312e95861311dc9b66c77dfc8
Flap-Py <saurav24081996@gmail.com>
2016-04-12 13:54:59 +0530

    addToDB func added

--- scm/src/scm/Tender.java
+++ scm/src/scm/Tender.java
@@ -1,11 +1,19 @@
 package scm;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Tender {
 	String tenderID;
 	String compName;
 	String desc;
 	String type;
 	String dateCreated;
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost:3306/SCM";
+	static final String USER = "root";
+	static final String PASS = "coincidence";
 	public String getType() {
 		return type;
 	}
@@ -37,7 +45,34 @@ public class Tender {
 		this.desc = desc;
 	}
 	public void addToDB(){
-		//
+		Connection conn = null;
+		java.sql.Statement stmt = null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			stmt = conn.createStatement();
+			String sql;
+			sql = "INSERT INTO Tenders VALUES('"+this.compName+"','"+this.tenderID+"','"+this.desc+"','"+"','"+this.type+"','"+this.dateCreated+"');";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			conn.close();
+		}catch(SQLException se1){     
+			se1.printStackTrace();
+		}catch(Exception e){          
+			e.printStackTrace();
+		}
+     	finally{
+     		try{
+     			if(stmt!=null)
+     				stmt.close();
+     		}catch(SQLException se2){}
+     		try{
+     			if(conn!=null)
+     				conn.close();
+     		}catch(SQLException se3){
+     			se3.printStackTrace();
+     		} 
+     	}
 	}
 	public void createtenderDB(){
 		//
--- scm/src/scm/User.java
+++ scm/src/scm/User.java
@@ -1,12 +1,19 @@
 package scm;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class User {
 	String compName;
 	String id;
 	String passwd;
 	String contact;
 	String address;
-	
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost:3306/SCM";
+	static final String USER = "root";
+	static final String PASS = "coincidence";
 	public User(String compName, String id, String passwd, String contact, String address) {
 		super();
 		this.compName = compName;
@@ -46,7 +53,34 @@ public class User {
 		this.address = address;
 	}
 	public void addToDB(){
-		//
+		Connection conn = null;
+		java.sql.Statement stmt = null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			stmt = conn.createStatement();
+			String sql;
+			sql = "INSERT INTO UserNames VALUES('"+this.compName+"','"+this.id+"','"+this.passwd+"','"+"','"+this.contact+"','"+this.address+"');";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			conn.close();
+		}catch(SQLException se1){     
+			se1.printStackTrace();
+		}catch(Exception e){          
+			e.printStackTrace();
+		}
+     	finally{
+     		try{
+     			if(stmt!=null)
+     				stmt.close();
+     		}catch(SQLException se2){}
+     		try{
+     			if(conn!=null)
+     				conn.close();
+     		}catch(SQLException se3){
+     			se3.printStackTrace();
+     		} 
+     	}
 	}
 	public void searchCompany(String Comp){
 		//
